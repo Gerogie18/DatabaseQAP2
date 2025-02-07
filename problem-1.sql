c72013c27005e1a09079577fe55469ae1221e9d5
Gerogie18 <jennlyver@gmail.com>
2025-02-07 19:46:56 -0330

    problem-1 insert data

--- problem-1.sql
+++ problem-1.sql
@@ -1,3 +1,4 @@
+--Create TABLES
 -- Create Student Table
 CREATE TABLE students (
     id SERIAL PRIMARY KEY,
@@ -30,3 +31,29 @@ CREATE TABLE enrollments (
     enrollment_date DATE NOT NULL,
     PRIMARY KEY (student_id, course_id)
 )
+
+-- INSERT Data
+-- Insert Students (5)
+INSERT INTO students (first_name, last_name, email, school_enrollment_date)
+VALUES
+    ('Brad', 'Majors', 'brad.majors@dentonhigh.edu', '2020-09-01'),
+    ('Janet', 'Weiss', 'janet.weiss@dentonhigh.edu', '2020-09-01'),
+    ('Riff', 'Raff', 'riff.raff@transylvaniaschool.edu', '2022-01-01'),
+    ('Columbia', 'Cantrell', 'columbia.cantrell@transylvaniaschool.edu', '2022-01-01'),
+    ('Eddie', 'Quinlan', 'eddie.quinlan@deliveryboy.edu', '2021-06-01');
+
+-- Insert Professors (4)
+INSERT INTO professors (first_name, last_name, department)
+VALUES
+    ('Frank-N-Furter', 'PhD', 'Theater'),
+    ('Dr. Everett', 'V. Scott', 'Science'),
+    ('Magenta', 'Usher', 'Music'),
+    ('The', 'Criminologist', 'Criminology');
+
+-- Insert courses (4)
+INSERT INTO courses (course_name, course_description, professor_id)
+VALUES
+    ('Sweet Transvestite Studies', 'An exploration of the intersection of identity, performance, and self-expression.', 1),
+    ('The Science of Reanimation', 'A study of the scientific principles behind bringing the dead back to life, with a focus on practical applications.', 2),
+    ('Time Warp Theory and Practice', 'An examination of the physics and philosophy of time travel, with a focus on the Time Warp as a case study.', 3),
+    ('Criminology of the Damned', 'A survey of the darker corners of human nature, with a focus on the crimes and misdeeds of the denizens of Transylvania.', 4);
